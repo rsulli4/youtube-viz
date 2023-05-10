3874b22203ad42ee2c5fa9eff45c14761e860be9
rsulli4 <rsulli4@gmu.edu>
2023-05-10 17:03:16 -0400

    updated intro

--- src/Introduction.js
+++ src/Introduction.js
@@ -2,5 +2,14 @@ export const Introduction = () =>{
     return <div className="Introduction">
         <h1>Youtube Data Analysis</h1>
         <h2>Ryan Sullivan, Huldah Merlin Kolli </h2>
+        <p>2.	Introduction:
+o	Problem Statement: The main goal is to bring more advanced analytics to the average Youtube user through use of Visualizations. 
+o	Impact: This is important for the average user or aspiring content creator to visualize other popular channels and give access to information not readily available to people outside of the channel owners.
+3.	Background: Google provides a robust set of api tools which allow for software developers to access their data provided. The usage of this requires in depth knowledge of api tools and most of this information is not available to or is restricted outside of these api’s. 
+4.	Method: We pull the data from the API and then using a react application create a visual dashboard of this data for specific channels which provides visualizations for several data points not readily available normally.
+5.	Methodology:
+o	Research Question(s): How can we improve access to YouTube data and leverage existing APIs to create a dashboard with more robust YouTube analytics. 
+o	Data Collection Process: The Data all comes Directly from YouTube’s Data V3 API which allows us to pull accurate real time data directly from the source.
+</p>
     </div>
 }
