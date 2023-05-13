1156fa13cc21e1e543158e3c348d252b67af6f24
rsulli4 <rsulli4@gmu.edu>
2023-05-13 09:37:23 -0400

    added about page

--- src/Introduction.js
+++ src/Introduction.js
@@ -2,5 +2,6 @@ export const Introduction = () =>{
     return <div className="Introduction">
         <h1>Youtube Data Analysis</h1>
         <h2>Ryan Sullivan, Huldah Merlin Kolli </h2>
+        <h3> <a href="/youtube-viz/blogPost.htm">About This App</a> </h3>
     </div>
 }
